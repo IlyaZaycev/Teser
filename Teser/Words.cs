b5705d9eb852863b2c02a6a203154cbf8f1ff699
IlyaZaycev <56030668+IlyaZaycev@users.noreply.github.com>
2022-07-03 21:57:00 +0400

    Привел таблицу в порядок

--- Teser/Words.cs
+++ Teser/Words.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
 using Word = Microsoft.Office.Interop.Word;
 using System.IO;
 using System.Linq;
@@ -21,30 +22,29 @@ namespace Teser
             #region Create Table
 
             doc.InsertParagraph();
-            Table table = doc.AddTable(15, 2);
+            Table table = doc.AddTable(14, 2);
             table.Alignment = Alignment.left;
-            table.Design = TableDesign.ColorfulList;
+            table.Design = TableDesign.TableGrid;
 
             table.Rows[0].Cells[0].Paragraphs.First().Append("№ п/п");
             table.Rows[0].Cells[1].Paragraphs.First().Append("-_-");
-
-            table.Rows[1].Cells[0].Paragraphs.First().Append("№ п/п");
-            table.Rows[2].Cells[0].Paragraphs.First().Append("Объект");
-            table.Rows[3].Cells[0].Paragraphs.First().Append("Кадастровый номер");
-            table.Rows[4].Cells[0].Paragraphs.First().Append("Действующая КС, руб.");
-            table.Rows[5].Cells[0].Paragraphs.First().Append("Дата определения КС");
-            table.Rows[6].Cells[0].Paragraphs.First().Append("Ставка налога/аренды %");
-            table.Rows[7].Cells[0].Paragraphs.First().Append("Налог на имущество, руб. в год");
-            table.Rows[8].Cells[0].Paragraphs.First().Append("Возможное снижение КС, руб.");
-            table.Rows[9].Cells[0].Paragraphs.First().Append("Налог на имущество при снижении КС, руб в год");
-            table.Rows[10].Cells[0].Paragraphs.First().Append("Экономия, руб. в год");
-            table.Rows[11].Cells[0].Paragraphs.First().Append(
+            
+            table.Rows[1].Cells[0].Paragraphs.First().Append("Объект");
+            table.Rows[2].Cells[0].Paragraphs.First().Append("Кадастровый номер");
+            table.Rows[3].Cells[0].Paragraphs.First().Append("Действующая КС, руб.");
+            table.Rows[4].Cells[0].Paragraphs.First().Append("Дата определения КС");
+            table.Rows[5].Cells[0].Paragraphs.First().Append("Ставка налога/аренды %");
+            table.Rows[6].Cells[0].Paragraphs.First().Append("Налог на имущество, руб. в год");
+            table.Rows[7].Cells[0].Paragraphs.First().Append("Возможное снижение КС, руб.");
+            table.Rows[8].Cells[0].Paragraphs.First().Append("Налог на имущество при снижении КС, руб в год");
+            table.Rows[9].Cells[0].Paragraphs.First().Append("Экономия, руб. в год");
+            table.Rows[10].Cells[0].Paragraphs.First().Append(
                 "Экономия ретроспектива, руб. в год(до 3 лет от даты определения КС до 01.01 текущего года)");
-            table.Rows[12].Cells[0].Paragraphs.First()
+            table.Rows[11].Cells[0].Paragraphs.First()
                 .Append("Экономия перспектива, руб. (за 3 года от 01.01 текущего года)");
-            table.Rows[13].Cells[0].Paragraphs.First()
+            table.Rows[12].Cells[0].Paragraphs.First()
                 .Append("Экономия перспектива, руб. (за 5 лет от 01.01 текущего года)");
-            table.Rows[14].Cells[0].Paragraphs.First().Append("Примечания");
+            table.Rows[13].Cells[0].Paragraphs.First().Append("Примечания");
 
 
             var abc = "";
@@ -53,7 +53,7 @@ namespace Teser
                 abc += "1";
                 var url = doc.AddHyperlink(abc, new Uri("https://www.youtube.com/"));
                 url.Text = abc;
-                table.Rows[i].Cells[1].Paragraphs.First().AppendHyperlink(url);
+                table.Rows[i].Cells[1].Paragraphs.First().AppendHyperlink(url).Bold().Color(Color.Aqua);
             }
 
             doc.InsertTable(table);
