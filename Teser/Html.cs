aa6f507a3d4f3ae19fd4256e6bf93824df82d1f2
IlyaZaycev <56030668+IlyaZaycev@users.noreply.github.com>
2022-07-03 18:15:39 +0400

    Создали html и текстовый документ

--- Teser/Html.cs
+++ Teser/Html.cs
@@ -1,77 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using HtmlAgilityPack;
 
 namespace Teser
 {
     public class Html
     {
-        // я в другом проекте пробовал взят ьинфу с сайта
-        // но не смог попробуй погуглить и может найдешь проблему
-        // сам сайт скачивается но в строку не записывается содержимое контейнера(div)
-        //может сможешь сделать с помощью API может доработаешь те варики которые я скинул сюда
-
-        const string classObject = "report-dictionary";
-        List<string> abc = new List<string>();
-        string url = @"https://egrnreestor.ru/search/18:26:010065:77?query=18%3A26%3A010065%3A77&metricId=7516441";
-
-        /*
-        HtmlWeb web = new HtmlWeb();
-        HtmlDocument doc = web.Load(url);
-        foreach (HtmlNode node in doc.DocumentNode.SelectNodes($"//div[@class='{classObject}']"))
+        public void CreateLinks()
         {
-            abc.Add( node.ChildNodes[0].InnerHtml);
+            string http =
+                $"http://www.udmbti.ru/?kadN=18%3A26%3A010065%3A77________&btn=Узнать+кадастровую+стоимость+объекта#top-c";
+            string l = "</span><br />Дата оценки: <span style=\"color: #3399ff;\">";
+            WebRequest req = WebRequest.Create(@"" + http);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string text = sr.ReadToEnd();
+            text = text.Substring(text.IndexOf(l), l.Length + 10);
+            text = text.Remove(0, l.Length);
+
+
+            Console.WriteLine(text);
         }
-
-        foreach (var a in abc)
-        {
-            Console.WriteLine(a);
-        }
-        */
-
-        /*
-        HtmlAgilityPack.HtmlDocument HD = new HtmlAgilityPack.HtmlDocument();
-        HD.LoadHtml(url);
-        var result = string.Empty;
-        result += HD.DocumentNode.SelectSingleNode($"//div[@class='{classObject}']//dd[@class='report-dictionary__value']").GetDirectInnerText();
-
-        Console.WriteLine(result);
-        */
-
-        /*
-        const string classObject = "report-dictionary__value";
-        string url = "https://egrnreestor.ru/search/18:26:010065:77?query=18%3A26%3A010065%3A77";
-
-        HtmlWeb webSite = new HtmlWeb();
-        HtmlDocument doc =  webSite.Load($"{url}");
-        var result = string.Empty;
-        result += doc.DocumentNode.SelectNodes("//div[contains(@class,'report-dictionary')]");
-        Console.WriteLine(result);
-        */
-
-
-        /*
-        HtmlWeb webSite = new HtmlWeb();
-        HtmlDocument document = webSite.Load("http://www.tibia.com/news/?subtopic=latestnews");
-        string content = document.GetElementbyId("PlayersOnline").OuterHtml;
-        //Console.WriteLine(content);
-        
-
-        /*
-         var web = new HtmlWeb();
-        web.OverrideEncoding = Encoding.UTF8;
-        HtmlDocument doc = web.Load(url);
-
-        List<string> abc = new List<string>();
-
-        HtmlNode node = doc.DocumentNode.SelectSingleNode($"//div[@class='{classObject}']");
-
-        abc.Add(node.ChildNodes[0].InnerHtml);
-
-
-        foreach (var a in abc)
-        {
-            Console.WriteLine(a);
-        }*/
     }
 }
---
+++ Teser/Words.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace Teser 
+{
+    public class Words
+    {
+        public void CreateWord()
+        {
+            var wordApplication = new Word.Application();
+            Object template = Type.Missing;
+            var newTemplate = Type.Missing;
+            var documentType = Type.Missing;
+            var visible = Type.Missing;
+            wordApplication.Documents.Add(ref template, ref newTemplate, ref documentType, ref visible);
+            var doc = wordApplication.ActiveDocument;
+            wordApplication.Visible = true;
+            //var r = Type.Missing;
+            //var par = doc.Content.Paragraphs.Add(ref r);
+            var missing = Type.Missing;
+            var rng = doc.Range(ref missing, ref missing);
+            //создание таблицы
+            rng.Tables.Add(doc.Paragraphs[doc.Paragraphs.Count].Range, 13, 2, ref missing, ref missing);
+            var tbl = doc.Tables[doc.Tables.Count];
+            //Отрисовка таблицы
+            var borders = new Word.Border[6];
+            borders[0] = tbl.Borders[Word.WdBorderType.wdBorderLeft];
+            borders[1] = tbl.Borders[Word.WdBorderType.wdBorderRight];
+            borders[2] = tbl.Borders[Word.WdBorderType.wdBorderTop];
+            borders[3] = tbl.Borders[Word.WdBorderType.wdBorderBottom];
+            borders[4] = tbl.Borders[Word.WdBorderType.wdBorderHorizontal];
+            borders[5] = tbl.Borders[Word.WdBorderType.wdBorderVertical];
+            foreach (Word.Border border in borders)
+            {
+                border.LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                border.Color = Word.WdColor.wdColorBlack;
+            }
+
+            tbl.Cell(1, 1).Range.Text = "№ п/п";
+            tbl.Cell(1, 2).Range.Text = "Объект";
+            tbl.Cell(2, 1).Range.Text = "Кадастровый номер";
+            tbl.Cell(3, 1).Range.Text = "Действующая КС, руб.";
+            tbl.Cell(4, 1).Range.Text = "Дата определения КС";
+            tbl.Cell(5, 1).Range.Text = "Ставка налога/аренды %";
+            tbl.Cell(6, 1).Range.Text = "Налог на имущество, руб. в год";
+            tbl.Cell(7, 1).Range.Text = "Возможное снижение КС, руб.";
+            tbl.Cell(8, 1).Range.Text = "Налог на имущество при снижении КС, руб в год";
+            tbl.Cell(9, 1).Range.Text = "Экономия, руб. в год";
+            tbl.Cell(10, 1).Range.Text =
+                "Экономия ретроспектива, руб. в год(до 3 лет от даты определения КС до 01.01 текущего года)";
+            tbl.Cell(11, 1).Range.Text = "Экономия перспектива, руб. (за 3 года от 01.01 текущего года)";
+            tbl.Cell(12, 1).Range.Text = "Экономия перспектива, руб. (за 5 лет от 01.01 текущего года)";
+            tbl.Cell(13, 1).Range.Text = "Примечания";
+
+            var adress = "https://yandex.ru/search/?text=Add+hyperlink+to+tableusing+word+Interop+word+c%23&lr=44";
+            Object qq = "123";
+            rng.Hyperlinks.Add(qq, adress);
+
+
+            qq = "1";
+            for (int i = 1; i < 13; i++)
+            {
+                qq += "+1";
+                tbl.Cell(i, 2).Range.Text = qq.ToString();
+            }
+        }
+        
+    }
+}
